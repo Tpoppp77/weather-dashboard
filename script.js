4121817b0ebea4210f72b8b8774bd6e4898a3da2
Tpoppp <olson0002@yahoo.com>
2020-08-11 17:50:20 -0700

    defined variables

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    var city = $("#input").val();
+    var now = moment().format("MMMM Do YYYY");
+    var currentTime = moment().format("HH:mm:ss");
+    var apiKey = "044c71fb82a90d43fcc214e917990441"
+    var cityList = [];
+    var storageList= [];
+    localStorage.getItem("cityList");
+
+
+});
